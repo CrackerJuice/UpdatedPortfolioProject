5507a87b62ba8f827762f29d1c160053fe31e713
CrackerJuice <Alex.marie3976@gmail.com>
2024-06-22 21:44:47 -0700

    Added files and started css.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import App from ",/App";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+    return (
+        <>
+        <Navbar />
+        <main className='mainPage'>
+            
+        </main>
+        </>
+    )
+}
+export default App
